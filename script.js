9d052c359594fbb1a4d8ec768f52b5dcd41dbb20
Ayushman <ayushmanchoudhary43@gmail.com>
2025-04-28 18:44:57 +0530

    newchanges

--- script.js
+++ script.js
@@ -7,83 +7,79 @@ let clearbtn=document.querySelector("#clear");
 
 //This code creates click event so that user can input text and adds to the to do list
 btn.addEventListener("click",()=>{
-    // div.appendChild(ul);
     if(userInput.value!=""){
         //code for creating lists
         let li=document.createElement("li");
 
-        //code for Creating checkboxes
+        //code for creating checkboxes
         let check=document.createElement("input");
         check.setAttribute("type","checkbox");
+        
         
           //Code For creating and styling Edit Button
           let edit=document.createElement("button");
           edit.innerText="Edit";
-          edit.style.fontSize="13px";
-          edit.style.backgroundColor="blue";
-          edit.style.color="white";
-          edit.style.padding="5px";
-          edit.style.border="none";
-          edit.style.width="55px";
-          edit.style.marginLeft="65px";
-          edit.style.borderRadius="20px";
+          edit.setAttribute("class","edit");
   
-
+        
         //code for creating and styling delete button
         let del=document.createElement("button");
         del.innerText="Delete";
-        del.style.fontSize="13px";
-        del.style.marginLeft="20px";
-        del.style.height="28px";
-        del.style.color="white";
-        del.style.width="55px";
-        del.style.border="none";
-        del.style.borderRadius="20px";
-        del.style.backgroundColor="red";
-        del.style.padding="5px";
+        del.setAttribute("class","delete");
 
-
-      
 
         //Styling list
-        li.style.listStyleType="none";
-        li.style.backgroundColor="aliceblue";
-        li.style.marginTop="9px";
-        li.style.marginBottom="5px";
-        li.style.borderRadius="12px";
-        li.style.padding="11px";
-        li.style.width="300px";
+        li.setAttribute("class","listyle");
 
         //This code handles userInput and add to the list
-        li.innerText="  "+userInput.value;
+        li.innerText= ' ' + userInput.value;
+        
+        //This code is used for displaying date and time to each list items added
+        let time=new Date();
+        let displaytime=document.createElement("h6");
+        displaytime.innerText=`Task added at  ${time.getDate()}/${time.getMonth()+1}/${time.getFullYear()}, ${time.getHours()}:${time.getMinutes()}`;
+        displaytime.style.color="white";
+        displaytime.style.marginTop="20px";
 
         //This code prepends checkbox to the list items
         li.prepend(check);
 
-        //This code appends edit and delete button to the list items
-        li.append(edit);
-        li.append(del);
-
-        //This code appends list items to make to-do list
-        ul.append(li);
         
+        
+        //This code appends list items to make to-do list and also displays date and time
+        div.append(displaytime);
+        div.append(li);
+
+
+        //This code appends edit and delete button to the list items
+        
+        div.append(edit);
+        div.append(del);
+        
+      
         //Code for editing list items
         edit.addEventListener("click",()=>{
             let editedvalue=prompt(`Enter a New Task`);
             if(editedvalue!="" && editedvalue!=null){
             li.innerText=" "+ editedvalue;
             li.prepend(check);
-            li.append(edit);
-            li.append(del);
+            div.append(edit);
+            div.append(del);
             }
         })
 
         //Code for deleting list items one by one
         del.addEventListener("click",()=>{
+            edit.remove();
+            del.remove();
+            displaytime.remove();
             li.remove();
         })
         //code for removing all list items using clear all button
         clearbtn.addEventListener("click",()=>{
+            edit.remove();
+            del.remove();
+            displaytime.remove();
             li.remove();
         })
         //Code that clears input screen
